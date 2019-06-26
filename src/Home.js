0bde753189e52378c362cb5efd2701db65028c77
Marc Diaz <marcgabe15@gmail.com>
2019-06-25 22:12:41 -0400

    added nav bar

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import './Home.css';
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="root">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className="menuButton" color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <h2 className="title">
+            Gator Pal
+          </h2>
+          <Button color="inherit">
+            <Link to="/login" className="link">
+              Login
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <h1>Home</h1>
+    </div> 
+  )
 };
 
 export default Home;
