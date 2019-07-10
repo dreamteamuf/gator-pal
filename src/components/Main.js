bb699e172c73872ab0c67dd594814708db7d73da
Marc Diaz <marcgabe15@gmail.com>
2019-07-09 22:17:38 -0400

    added swipeables

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SwipeableViews from 'react-swipeable-views'
 
 class Main extends Component {
     constructor(props) {
@@ -8,7 +9,11 @@ class Main extends Component {
     render() {
         return (
             <section id="Main">
-                Main
+            <SwipeableViews enableMouseEvents>
+                <div>Bro</div>
+                <div>What</div>
+                <div>Please</div>
+            </SwipeableViews>
             </section>
         );
     }
