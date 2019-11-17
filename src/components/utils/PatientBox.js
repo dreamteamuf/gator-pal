2ac1ba5b653f84121d1b5b4a926a935165197092
mohona17 <mohonaahmed@ufl.edu>
2019-11-16 23:08:44 -0500

    Revert "Deleting deprecated file"
    This reverts commit 9cc887d11bfe84fbaffea8cacb7e59c476d2397c.

---
+++ src/components/utils/PatientBox.js
@@ -0,0 +1,16 @@
+import React, { Component }from "react";
+import Paper from '@material-ui/core/Paper'
+
+
+class PatientBox extends Component {
+    
+    render() {
+        return (
+        <Paper>
+            Hello
+        </Paper>
+        )
+    }
+}
+
+export default PatientBox;
